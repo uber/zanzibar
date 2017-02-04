e0b6f5a08a4d798ba23cb3a8440fb80e0286c5bd
Michael Sindelar <sindelar@uber.com>
2017-02-03 17:23:57 -0800

    Autogenerate benchmarks

---
+++ examples/example-gateway/test_goldens/foo.go
@@ -0,0 +1,4 @@
+package test_goldens
+
+var request = ""
+var response = ""
---
+++ lib/gencode/templates/foo/foo.thrift
@@ -0,0 +1,13 @@
+namespace java com.uber.zanzibar.clients.foo
+
+struct FooName {
+    1: optional string name
+}
+struct FooStruct {
+    1: required string fooString
+    2: optional i32 fooI32
+    3: optional i16 fooI16
+    4: optional double fooDouble
+    5: optional bool fooBool
+    6: optional map<string, FooName> fooMap
+}
