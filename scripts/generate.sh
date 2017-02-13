d7c34502c6c6167904237e4a2d00095aad7fba97
Raynos <jakev@uber.com>
2017-02-13 12:55:03 -0800

    scripts/generate: cleanup file

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,10 +5,10 @@ go get go.uber.org/thriftrw
 echo "Generating Go code from Thrift files"
 rm -rf "$PREFIX/gen-code"
 mkdir "$PREFIX/gen-code"
-for tfile in $(find "$PREFIX/idl/github.com" -name '*.thrift'); do
+for tfile in $(find "$PREFIX/idl" -name '*.thrift'); do
     thriftrw \
         --out="$PREFIX/gen-code" \
-        --thrift-root="$PREFIX/idl/github.com" \
+        --thrift-root="$PREFIX/idl" \
         --no-service-helpers "$tfile"
 done
 
@@ -17,24 +17,13 @@ for file in $(find "$PREFIX/gen-code" -name 'versioncheck.go'); do
 done
 
 echo "Generating JSON Marshal/Unmarshal"
-for file in $(find "$PREFIX/gen-code" -name "*.go" | grep -v "versioncheck.go"); do
+for file in $(find "$PREFIX/gen-code" -name "*.go" | grep -v "versioncheck.go");do
     ./scripts/easy_json/easy_json "$file"
 done
 
-
-#for file in $(find "$PREFIX/gen-code" -name "*_easyjson.go"); do
-    #sed -E "/^package (\w)+$/a\ // @generated/" "$file" > "$file"
-    #git clean -qf '*.bak'
-#done
-
-#for file in $(find "$PREFIX" -name "*_easyjson.go"); do
-    #sed -E "/^package (\w)+$/a\ // @generated/" "$file" > "$file"
-    #git clean -qf '*.bak'
-#done
-
 echo "Generating Endpoint Handlers"
 mkdir "$PREFIX/gen-code/handlers"
-for file in $(find "$PREFIX/gen-code/uber/zanzibar/endpoints/bar" -name "types.go" | grep -v "versioncheck.go"); do
+for file in $(find "$PREFIX/gen-code/github.com/uber/zanzibar/endpoints/bar" -name "types.go" | grep -v "versioncheck.go"); do
     go run codegen/runner/main.go $PREFIX/gen-code/handlers $file
 done
 
