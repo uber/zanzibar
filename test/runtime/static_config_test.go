825df15f90da9c88cfcaa1e5ad1d8178ed83fd3b
Raynos <jakev@uber.com>
2017-02-16 16:06:22 -0800

    write tests with 100% coverage

---
+++ test/runtime/static_config_test.go
@@ -0,0 +1,563 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package runtime_test
+
+import (
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"encoding/json"
+
+	"io/ioutil"
+
+	"github.com/stretchr/testify/assert"
+	zanzibar "github.com/uber/zanzibar/runtime"
+)
+
+var testDir string = filepath.Join(getProjectDir(), "test", "runtime")
+
+func getProjectDir() string {
+	goPath := os.Getenv("GOPATH")
+	return path.Join(goPath, "src", "github.com", "uber", "zanzibar")
+}
+
+func TestEmptyConfig(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(nil, nil)
+
+	config.SetOrDie("k", "v")
+	assert.Equal(t, config.MustGetString("k"), "v")
+
+	config.SetOrDie("k2", true)
+	assert.Equal(t, config.MustGetBoolean("k2"), true)
+
+	config.SetOrDie("k3", int64(4))
+	assert.Equal(t, config.MustGetInt("k3"), int64(4))
+
+	config.SetOrDie("k4", float64(4.0))
+	assert.Equal(t, config.MustGetFloat("k4"), float64(4.0))
+}
+
+func TestGetNamespace(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(nil, nil)
+
+	config.SetOrDie("a.b.c", "v")
+	assert.Equal(t, config.MustGetString("a.b.c"), "v")
+}
+
+func TestPanicNonExistantKeys(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(nil, nil)
+
+	assert.Panics(t, func() {
+		config.MustGetString("a.b.c")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetBoolean("a.b.c")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetInt("a.b.c")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetFloat("a.b.c")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetStruct("a.b.c", nil)
+	})
+}
+
+func TestPanicGetWrongTypes(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(
+		[]string{},
+		map[string]interface{}{
+			"a.b.c": "v",
+			"bool":  true,
+			"int":   int64(1),
+			"float": float64(1.0),
+		},
+	)
+
+	assert.Panics(t, func() {
+		config.MustGetBoolean("int")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetFloat("bool")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetInt("a.b.c")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetString("bool")
+	})
+}
+
+func TestSupportsSeedConfig(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(
+		[]string{},
+		map[string]interface{}{
+			"a.b.c": "v",
+			"bool":  true,
+			"int":   int64(1),
+			"float": float64(1.0),
+		},
+	)
+
+	assert.Equal(t, config.MustGetString("a.b.c"), "v")
+	assert.Equal(t, config.MustGetBoolean("bool"), true)
+	assert.Equal(t, config.MustGetInt("int"), int64(1))
+	assert.Equal(t, config.MustGetFloat("float"), float64(1.0))
+}
+
+func TestCannotSetExistingKeys(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(
+		[]string{},
+		map[string]interface{}{
+			"a.b.c": "v",
+		},
+	)
+
+	assert.Panics(t, func() {
+		config.SetOrDie("a.b.c", "x")
+	})
+}
+
+func TestCannotGetFromDestroyedConfig(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(
+		[]string{},
+		map[string]interface{}{
+			"a.b.c": "v",
+			"bool":  true,
+			"int":   int64(1),
+			"float": float64(1.0),
+		},
+	)
+
+	assert.Equal(t, config.MustGetString("a.b.c"), "v")
+	assert.Equal(t, config.MustGetBoolean("bool"), true)
+	assert.Equal(t, config.MustGetInt("int"), int64(1))
+	assert.Equal(t, config.MustGetFloat("float"), float64(1.0))
+
+	config.Destroy()
+
+	assert.Panics(t, func() {
+		config.MustGetString("a.b.c")
+	})
+
+	assert.Panics(t, func() {
+		assert.Equal(t, config.MustGetBoolean("bool"), true)
+	})
+
+	assert.Panics(t, func() {
+		assert.Equal(t, config.MustGetInt("int"), int64(1))
+	})
+
+	assert.Panics(t, func() {
+		assert.Equal(t, config.MustGetFloat("float"), float64(1.0))
+	})
+
+	assert.Panics(t, func() {
+		var x bool
+		config.MustGetStruct("bool", &x)
+		assert.Equal(t, x, true)
+	})
+}
+
+func TestCannotSetOnFrozenConfig(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(nil, nil)
+
+	config.SetOrDie("a", "a")
+	config.SetOrDie("b", "b")
+
+	config.Freeze()
+
+	assert.Panics(t, func() {
+		config.SetOrDie("c", "c")
+	})
+
+	assert.Equal(t, config.InspectOrDie(), map[string]interface{}{
+		"a": "a",
+		"b": "b",
+	})
+}
+
+func mustMarshal(v interface{}) []byte {
+	bytes, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+
+	return bytes
+}
+
+type fixtureWriter struct {
+	rootDir  string
+	fixtures map[string][]byte
+}
+
+func WriteFixture(rootDir string, fixtures map[string][]byte) io.Closer {
+	writer := &fixtureWriter{
+		rootDir:  rootDir,
+		fixtures: fixtures,
+	}
+
+	writer.writeToDisk()
+
+	return writer
+}
+
+func (writer *fixtureWriter) writeToDisk() {
+	for fileName, bytes := range writer.fixtures {
+		filePath := filepath.Join(writer.rootDir, fileName)
+
+		err := os.MkdirAll(filepath.Dir(filePath), 0755)
+		if err != nil {
+			panic(err)
+		}
+		err = ioutil.WriteFile(filePath, bytes, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (writer *fixtureWriter) Close() error {
+	for fileName, _ := range writer.fixtures {
+		parts := strings.Split(fileName, "/")
+		filePath := filepath.Join(writer.rootDir, parts[0])
+
+		err := os.RemoveAll(filePath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return nil
+}
+
+func TestCanReadFromFile(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]interface{}{
+			"a":     "b",
+			"a.b.c": "v",
+			"bool":  true,
+			"int":   int64(1),
+			"float": float64(1.0),
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, nil)
+
+	assert.Equal(t, config.MustGetString("a"), "b")
+	assert.Equal(t, config.MustGetString("a.b.c"), "v")
+	assert.Equal(t, config.MustGetBoolean("bool"), true)
+	assert.Equal(t, config.MustGetInt("int"), int64(1))
+	assert.Equal(t, config.MustGetFloat("float"), float64(1.0))
+
+	closer.Close()
+}
+
+func TestCannotSetOverValueFromFile(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]interface{}{
+			"a":     "b",
+			"a.b.c": "v",
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, nil)
+
+	assert.Panics(t, func() {
+		config.SetOrDie("a", "c")
+	})
+
+	closer.Close()
+}
+
+func TestReadFromSeedConfigIntoNil(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie(nil, map[string]interface{}{
+		"a": "c",
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetStruct("a", nil)
+	})
+}
+
+func TestSeedConfigOverwritesFiles(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]string{
+			"a":     "b",
+			"a.b.c": "v",
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, map[string]interface{}{
+		"a": "c",
+	})
+
+	assert.Equal(t, config.MustGetString("a"), "c")
+	assert.Equal(t, config.MustGetString("a.b.c"), "v")
+
+	closer.Close()
+}
+
+func TestLaterFilesOverwriteEarlierFiles(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]string{
+			"a":     "b",
+			"a.b.c": "v",
+		}),
+		"config/local.json": mustMarshal(map[string]string{
+			"a": "c",
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+		filepath.Join(testDir, "config", "local.json"),
+	}, nil)
+
+	assert.Equal(t, config.MustGetString("a"), "c")
+	assert.Equal(t, config.MustGetString("a.b.c"), "v")
+
+	closer.Close()
+}
+
+func TestSupportsNonExistantFiles(t *testing.T) {
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+		filepath.Join(testDir, "config", "local.json"),
+	}, map[string]interface{}{
+		"a":     "d",
+		"a.b.c": "v2",
+	})
+
+	assert.Equal(t, config.MustGetString("a"), "d")
+	assert.Equal(t, config.MustGetString("a.b.c"), "v2")
+}
+
+func TestThrowsForInvalidJSON(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]string{
+			"a":     "b",
+			"a.b.c": "v",
+		}),
+		"config/local.json": []byte("{...}"),
+	})
+
+	assert.Panics(t, func() {
+		_ = zanzibar.NewStaticConfigOrDie([]string{
+			filepath.Join(testDir, "config", "production.json"),
+			filepath.Join(testDir, "config", "local.json"),
+		}, nil)
+	})
+
+	closer.Close()
+}
+
+func TestThrowsForReadingBadFiles(t *testing.T) {
+	assert.Panics(t, func() {
+		_ = zanzibar.NewStaticConfigOrDie([]string{
+			filepath.Join(testDir),
+		}, nil)
+	})
+}
+
+func TestGetStructFromDisk(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]interface{}{
+			"a": map[string]interface{}{
+				"Field": "a",
+				"Foo":   4,
+			},
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, nil)
+
+	var m struct {
+		Field string
+		Foo   int
+	}
+	config.MustGetStruct("a", &m)
+	assert.Equal(t, m.Field, "a")
+	assert.Equal(t, m.Foo, 4)
+
+	closer.Close()
+}
+
+func TestInspectMalformedDataFromDisk(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": []byte(
+			"{ \"a\": { \"c\": ... }, \"b\": \"c\" }",
+		),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, nil)
+
+	assert.Panics(t, func() {
+		config.InspectOrDie()
+	})
+
+	closer.Close()
+}
+
+func TestReadStructIntoWrongType(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]interface{}{
+			"a": true,
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, nil)
+
+	var m struct {
+		Field string
+		Foo   int
+	}
+	assert.Panics(t, func() {
+		config.MustGetStruct("a", &m)
+	})
+
+	closer.Close()
+}
+
+func TestOverwriteStructFromSeedConfig(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]interface{}{
+			"a": map[string]interface{}{
+				"Field": "a",
+				"Foo":   4,
+			},
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, map[string]interface{}{
+		"a": struct {
+			Field string
+			Foo   int
+		}{
+			Field: "b",
+			Foo:   5,
+		},
+	})
+
+	var m struct {
+		Field string
+		Foo   int
+	}
+	config.MustGetStruct("a", &m)
+	assert.Equal(t, m.Field, "b")
+	assert.Equal(t, m.Foo, 5)
+
+	closer.Close()
+}
+
+func TestInspectFromFile(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]interface{}{
+			"a":     "b",
+			"a.b.c": "v",
+			"bool":  true,
+			"int":   int64(1),
+			"float": float64(1.0),
+			"struct": map[string]interface{}{
+				"Field": "a",
+				"Foo":   4,
+			},
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, nil)
+
+	assert.Equal(t, config.InspectOrDie(), map[string]interface{}{
+		"a":     "b",
+		"a.b.c": "v",
+		"bool":  true,
+		"float": float64(1.0),
+		"int":   float64(1),
+		"struct": map[string]interface{}{
+			"Field": "a",
+			"Foo":   float64(4.0),
+		},
+	})
+
+	closer.Close()
+}
+
+func TestPanicReadingWrongTypeFromDisk(t *testing.T) {
+	closer := WriteFixture(testDir, map[string][]byte{
+		"config/production.json": mustMarshal(map[string]interface{}{
+			"a":     "b",
+			"a.b.c": "v",
+			"bool":  true,
+			"int":   int64(1),
+			"float": float64(1.0),
+		}),
+	})
+
+	config := zanzibar.NewStaticConfigOrDie([]string{
+		filepath.Join(testDir, "config", "production.json"),
+	}, nil)
+
+	assert.Panics(t, func() {
+		config.MustGetBoolean("a")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetString("int")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetInt("a.b.c")
+	})
+
+	assert.Panics(t, func() {
+		config.MustGetFloat("bool")
+	})
+
+	closer.Close()
+}
