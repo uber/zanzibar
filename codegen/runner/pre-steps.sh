076a6053a3754766e7e2dbf05bff0262d2843be3
Raynos <jakev@uber.com>
2017-02-22 11:52:26 -0800

    codegen/pre-steps: support seperate build & idl dirs

--- codegen/runner/pre-steps.sh
+++ codegen/runner/pre-steps.sh
@@ -2,11 +2,17 @@
 set -e
 
 if [ -z $1 ]; then
-	echo "prefix argument (\$1) is missing"
+	echo "build dir argument (\$1) is missing"
 	exit 1
 fi
 
-PREFIX="$1"
+if [ -z $2 ]; then
+	echo "config dir argument (\$2) is missing"
+	exit 1
+fi
+
+BUILD_DIR="$1"
+CONFIG_DIR="$2"
 DIRNAME="$(dirname $0)"
 EASY_JSON_RAW_DIR="$DIRNAME/../../scripts/easy_json"
 EASY_JSON_DIR="`cd "${EASY_JSON_RAW_DIR}";pwd`"
@@ -14,19 +20,19 @@ EASY_JSON_FILE="$EASY_JSON_DIR/easy_json.go"
 
 go get -u go.uber.org/thriftrw
 echo "Generating Go code from Thrift files"
-rm -rf "$PREFIX/gen-code"
-mkdir "$PREFIX/gen-code"
-for tfile in $(find "$PREFIX/idl" -name '*.thrift'); do
+rm -rf "$BUILD_DIR/gen-code"
+mkdir "$BUILD_DIR/gen-code"
+for tfile in $(find "$CONFIG_DIR/idl" -name '*.thrift'); do
     thriftrw \
-        --out="$PREFIX/gen-code" \
-        --thrift-root="$PREFIX/idl" \
+        --out="$BUILD_DIR/gen-code" \
+        --thrift-root="$CONFIG_DIR/idl" \
         --no-service-helpers "$tfile"
 done
-for file in $(find "$PREFIX/gen-code" -name 'versioncheck.go'); do
+for file in $(find "$BUILD_DIR/gen-code" -name 'versioncheck.go'); do
     rm "$file"
 done
 
 echo "Generating JSON Marshal/Unmarshal"
-for file in $(find "$PREFIX/gen-code" -name "*.go" | grep -v "versioncheck.go");do
+for file in $(find "$BUILD_DIR/gen-code" -name "*.go" | grep -v "versioncheck.go");do
     go run "$EASY_JSON_FILE" -- "$file"
 done
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PREFIX=examples/example-gateway
 
-bash ./codegen/runner/pre-steps.sh "$PREFIX"
+bash ./codegen/runner/pre-steps.sh "$PREFIX" "$PREFIX"
 go run codegen/runner/runner.go -config="$PREFIX/gateway.json"
 bash ./codegen/runner/post-steps.sh "$PREFIX"
