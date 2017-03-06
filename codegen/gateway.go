25dec14994cddde4b3bcb10caa0e5c94d89dfb02
Raynos <raynos2@gmail.com>
2017-03-06 13:39:27 -0800

    codegen: do not generate endpoint for custom endpoints

--- codegen/gateway.go
+++ codegen/gateway.go
@@ -418,6 +418,10 @@ func (gateway *GatewaySpec) GenerateClients() error {
 // GenerateEndpoints will generate all the endpoints for the gateway
 func (gateway *GatewaySpec) GenerateEndpoints() error {
 	for _, module := range gateway.EndpointModules {
+		if module.WorkflowType == "custom" {
+			continue
+		}
+
 		_, err := gateway.Template.GenerateEndpointFile(
 			module, gateway.PackageHelper,
 		)
