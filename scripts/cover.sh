029c7e34f84f85b63fce681018daca7bb20c1463
Raynos <raynos2@gmail.com>
2017-03-17 18:27:33 -0700

    scripts/cover.sh: also run tests in examples folder

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -12,7 +12,7 @@ COVER_PKGS=$(glide novendor | grep -v "test/..." | \
 	grep -v "main/..." | grep -v "benchmarks/..." | \
 	awk -v ORS=, '{ print $1 }' | sed 's/,$/\n/')
 
-FILES=$(go list ./... | grep -v "vendor" | grep "test")
+FILES=$(go list ./... | grep -v "vendor" | grep "test\|examples")
 FILES_ARR=($FILES)
 
 for file in "${FILES_ARR[@]}"; do
