2d456285d86eb8a1192fbc79bb597b8ea01d55f0
Zhenghui Wang <zw@uber.com>
2017-03-31 15:05:16 -0700

    use best ip on localhost

--- examples/example-gateway/build/main.go
+++ examples/example-gateway/build/main.go
@@ -9,6 +9,7 @@ import (
 	"runtime"
 
 	"github.com/uber-go/zap"
+	tchannel "github.com/uber/tchannel-go"
 	"github.com/uber/zanzibar/examples/example-gateway/build/clients"
 	"github.com/uber/zanzibar/examples/example-gateway/build/endpoints"
 	"github.com/uber/zanzibar/runtime"
@@ -27,16 +28,22 @@ func getConfigDirName() string {
 	)
 }
 
-func getConfig() *zanzibar.StaticConfig {
+func getConfig(seedConfig map[string]interface{}) *zanzibar.StaticConfig {
 	return zanzibar.NewStaticConfigOrDie([]string{
 		filepath.Join(getDirName(), "zanzibar-defaults.json"),
 		filepath.Join(getConfigDirName(), "production.json"),
 		filepath.Join(os.Getenv("CONFIG_DIR"), "production.json"),
-	}, nil)
+	}, seedConfig)
 }
 
 func createGateway() (*zanzibar.Gateway, error) {
-	config := getConfig()
+	listenIP, err := tchannel.ListenIP()
+	if err != nil {
+		return nil, err
+	}
+	config := getConfig(map[string]interface{}{
+		"ip": listenIP.String(),
+	})
 	clients := clients.CreateClients(config)
 	return zanzibar.CreateGateway(config, &zanzibar.Options{
 		Clients: clients,
