f936dc162b581aba5cbcecc6e085cbd26ef81727
Raynos <raynos2@gmail.com>
2017-03-31 17:25:34 -0700

    example-gateway: update handwritten contacts endpoint

--- examples/example-gateway/endpoints/contacts/save-contacts.go
+++ examples/example-gateway/endpoints/contacts/save-contacts.go
@@ -32,7 +32,7 @@ func HandleSaveContactsRequest(
 	body.AppVersion = req.Header.Get("x-uber-client-version")
 
 	clientBody := convertToClient(&body)
-	cres, err := clients.Contacts.SaveContacts(ctx, clientBody)
+	cres, _, err := clients.Contacts.SaveContacts(ctx, nil, clientBody)
 	if err != nil {
 		req.Logger.Error("Could not make client request",
 			zap.String("error", err.Error()),
