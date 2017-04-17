0952916b794aabefdbd3f73e7834297fa77d8499
Michael Sindelar <sindelar@uber.com>
2017-04-17 16:25:27 -0700

    add one more gen file

--- examples/example-gateway/build/endpoints/googlenow/googlenow_googlenow_method_addcredentials.go
+++ examples/example-gateway/build/endpoints/googlenow/googlenow_googlenow_method_addcredentials.go
@@ -62,7 +62,7 @@ func (w AddCredentialsEndpoint) Handle(
 	clientRequest := convertToAddCredentialsClientRequest(r)
 
 	clientHeaders := map[string]string{}
-	for k, v := range map[string]string{"x-uuid": "x-uuid", "x-token": "x-token"} {
+	for k, v := range map[string]string{"x-token": "x-token", "x-uuid": "x-uuid"} {
 		clientHeaders[v] = headers.Get(k)
 	}
 
@@ -78,7 +78,7 @@ func (w AddCredentialsEndpoint) Handle(
 
 	// Filter and map response headers from client to server response.
 	endRespHead := map[string]string{}
-	for k, v := range map[string]string{"x-token": "x-token", "x-uuid": "x-uuid"} {
+	for k, v := range map[string]string{"x-uuid": "x-uuid", "x-token": "x-token"} {
 		endRespHead[v] = respHeaders[k]
 	}
 
