00917cd09eec098e9f6a896e702cdb0362c8906d
Michael Sindelar <sindelar@uber.com>
2017-04-21 13:10:47 -0700

    Rename ServerHeaderInterface to ServerHeader

--- runtime/server_header.go
+++ runtime/server_header.go
@@ -24,8 +24,8 @@ import (
 	"net/http"
 )
 
-// ServerHeaderInterface defines methods on ServerHeaders
-type ServerHeaderInterface interface {
+// ServerHeader defines methods on ServerHeaders
+type ServerHeader interface {
 	Get(key string) (string, bool)
 	Add(key string, value string)
 	Set(key string, value string)
