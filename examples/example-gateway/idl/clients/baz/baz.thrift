83f34adad3cb7ed07458d68451bf4116488491e0
Raynos <raynos2@gmail.com>
2017-05-24 16:10:12 -0700

    example-gateway/idl: fix whitespace

--- examples/example-gateway/idl/clients/baz/baz.thrift
+++ examples/example-gateway/idl/clients/baz/baz.thrift
@@ -2,39 +2,39 @@ namespace java com.uber.zanzibar.clients.baz
 include "base.thrift"
 
 struct BazRequest {
-  1: required bool b1
-  2: required string s2
-  3: required i32 i3
+    1: required bool b1
+    2: required string s2
+    3: required i32 i3
 }
 
 exception AuthErr {
-  1: required string message
+    1: required string message
 }
 
 service SimpleService {
-  base.BazResponse Compare(
-    1: required BazRequest arg1
-    2: required BazRequest arg2
-  ) throws (
-    1: AuthErr authErr
-  )
+    base.BazResponse Compare(
+        1: required BazRequest arg1
+        2: required BazRequest arg2
+    ) throws (
+        1: AuthErr authErr
+    )
 
-  void Call(
-    1: required BazRequest arg
-  ) throws (
-    1: AuthErr authErr
-  )
+    void Call(
+        1: required BazRequest arg
+    ) throws (
+        1: AuthErr authErr
+    )
 
-  base.BazResponse Ping() ()
+    base.BazResponse Ping() ()
 
-  void SillyNoop() throws (
-    1: AuthErr authErr
-    2: base.ServerErr serverErr
-  )
+    void SillyNoop() throws (
+        1: AuthErr authErr
+        2: base.ServerErr serverErr
+    )
 }
 
 service SecondService {
-  void Echo(
-    1: required string arg
-  )
+    void Echo(
+        1: required string arg
+    )
 }
--- examples/example-gateway/idl/endpoints/bar/bar.thrift
+++ examples/example-gateway/idl/endpoints/bar/bar.thrift
@@ -3,80 +3,82 @@ namespace java com.uber.zanzibar.clients.bar
 include "../foo/foo.thrift"
 
 struct BarRequest {
-  1: required string stringField (zanzibar.http.ref = "params.someParamsField")
-  2: required bool boolField (zanzibar.http.ref = "query.some-query-field")
+    1: required string stringField (zanzibar.http.ref = "params.someParamsField")
+    2: required bool boolField (zanzibar.http.ref = "query.some-query-field")
 }
 struct BarResponse {
-  1: required string stringField (
-    zanzibar.http.ref = "headers.some-header-field"
-    zanzibar.validation.type = "object,number"
-  )
-  2: required i32 intWithRange
-  3: required i32 intWithoutRange (zanzibar.ignore.integer.range = "true")
-  4: required map<string, i32> mapIntWithRange
-  5: required map<string, i32> mapIntWithoutRange (zanzibar.ignore.integer.range = "true")
+    1: required string stringField (
+        zanzibar.http.ref = "headers.some-header-field"
+        zanzibar.validation.type = "object,number"
+    )
+    2: required i32 intWithRange
+    3: required i32 intWithoutRange (zanzibar.ignore.integer.range = "true")
+    4: required map<string, i32> mapIntWithRange
+    5: required map<string, i32> mapIntWithoutRange (
+        zanzibar.ignore.integer.range = "true"
+    )
 }
 
 exception BarException {
-  1: required string stringField (zanzibar.http.ref = "headers.another-header-field")
+    1: required string stringField (zanzibar.http.ref = "headers.another-header-field")
 }
 
 service Bar {
-  BarResponse normal (
-    1: required BarRequest request
-  ) throws (
-    1: BarException barException (zanzibar.http.status = "403")
-  ) (
-    zanzibar.http.method = "POST"
-    zanzibar.http.path = "/bar/bar-path"
-    zanzibar.http.status = "200"
-    zanzibar.meta = "SomeMeta"
-    zanzibar.handler = "bar.baz"
-  )
-  BarResponse noRequest (
-  ) throws (
-    1: BarException barException (zanzibar.http.status = "403")
-  ) (
-    zanzibar.http.method = "GET"
-    zanzibar.http.path = "/bar/no-request-path"
-    zanzibar.http.status = "200"
-    zanzibar.meta = "SomeMeta"
-    zanzibar.handler = "bar.baz"
-  )
-  BarResponse missingArg (
-  ) throws (
-    1: BarException barException (zanzibar.http.status = "403")
-  ) (
-    zanzibar.http.method = "GET"
-    zanzibar.http.path = "/bar/missing-arg-path"
-    zanzibar.http.status = "200"
-    zanzibar.meta = "SomeMeta"
-    zanzibar.handler = "bar.baz"
-  )
-  BarResponse tooManyArgs (
-    1: required BarRequest request
-    2: optional foo.FooStruct foo
-  ) throws (
-    1: BarException barException (zanzibar.http.status = "403")
-  ) (
-    zanzibar.http.headers = "x-uuid,x-token"
-    zanzibar.http.method = "POST"
-    zanzibar.http.path = "/bar/too-many-args-path"
-    zanzibar.http.status = "200"
-    zanzibar.meta = "SomeMeta"
-    zanzibar.handler = "bar.baz"
-    zanzibar.http.reqHeaders = "x-uuid,x-token"
-    zanzibar.http.resHeaders = "x-uuid,x-token"
-  )
-  void argNotStruct (
-    1: required string request
-  ) throws (
-    1: BarException barException (zanzibar.http.status = "403")
-  ) (
-    zanzibar.http.method = "POST"
-    zanzibar.http.path = "/bar/arg-not-struct-path"
-    zanzibar.http.status = "200"
-    zanzibar.meta = "SomeMeta"
-    zanzibar.handler = "bar.baz"
-  )
+    BarResponse normal (
+        1: required BarRequest request
+    ) throws (
+        1: BarException barException (zanzibar.http.status = "403")
+    ) (
+        zanzibar.http.method = "POST"
+        zanzibar.http.path = "/bar/bar-path"
+        zanzibar.http.status = "200"
+        zanzibar.meta = "SomeMeta"
+        zanzibar.handler = "bar.baz"
+    )
+    BarResponse noRequest (
+    ) throws (
+        1: BarException barException (zanzibar.http.status = "403")
+    ) (
+        zanzibar.http.method = "GET"
+        zanzibar.http.path = "/bar/no-request-path"
+        zanzibar.http.status = "200"
+        zanzibar.meta = "SomeMeta"
+        zanzibar.handler = "bar.baz"
+    )
+    BarResponse missingArg (
+    ) throws (
+        1: BarException barException (zanzibar.http.status = "403")
+    ) (
+        zanzibar.http.method = "GET"
+        zanzibar.http.path = "/bar/missing-arg-path"
+        zanzibar.http.status = "200"
+        zanzibar.meta = "SomeMeta"
+        zanzibar.handler = "bar.baz"
+    )
+    BarResponse tooManyArgs (
+        1: required BarRequest request
+        2: optional foo.FooStruct foo
+    ) throws (
+        1: BarException barException (zanzibar.http.status = "403")
+    ) (
+        zanzibar.http.headers = "x-uuid,x-token"
+        zanzibar.http.method = "POST"
+        zanzibar.http.path = "/bar/too-many-args-path"
+        zanzibar.http.status = "200"
+        zanzibar.meta = "SomeMeta"
+        zanzibar.handler = "bar.baz"
+        zanzibar.http.reqHeaders = "x-uuid,x-token"
+        zanzibar.http.resHeaders = "x-uuid,x-token"
+    )
+    void argNotStruct (
+        1: required string request
+    ) throws (
+        1: BarException barException (zanzibar.http.status = "403")
+    ) (
+        zanzibar.http.method = "POST"
+        zanzibar.http.path = "/bar/arg-not-struct-path"
+        zanzibar.http.status = "200"
+        zanzibar.meta = "SomeMeta"
+        zanzibar.handler = "bar.baz"
+    )
 }
