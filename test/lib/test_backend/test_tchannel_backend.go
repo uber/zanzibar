a73097cc2bb1bf74a475c195d447f420422f60d9
Michael Sindelar <sindelar@uber.com>
2017-06-06 18:36:45 -0700

    Name->ID

--- test/lib/test_backend/test_tchannel_backend.go
+++ test/lib/test_backend/test_tchannel_backend.go
@@ -48,13 +48,13 @@ func BuildTChannelBackends(
 	result := make(map[string]*TestTChannelBackend, n)
 
 	for i := 0; i < n; i++ {
-		clientName := knownTChannelBackends[i]
+		clientID := knownTChannelBackends[i]
 
-		val, ok := cfg["clients."+clientName+".serviceName"]
+		val, ok := cfg["clients."+clientID+".serviceName"]
 		var serviceName string
 		if !ok {
-			serviceName = clientName
-			cfg["clients."+clientName+".serviceName"] = serviceName
+			serviceName = clientID
+			cfg["clients."+clientID+".serviceName"] = serviceName
 		} else {
 			serviceName = val.(string)
 		}
@@ -69,11 +69,11 @@ func BuildTChannelBackends(
 			return nil, err
 		}
 
-		result[clientName] = backend
-		cfg["clients."+clientName+".ip"] = "127.0.0.1"
-		cfg["clients."+clientName+".port"] = int64(backend.RealPort)
-		cfg["clients."+clientName+".timeout"] = int64(10000)
-		cfg["clients."+clientName+".timeoutPerAttempt"] = int64(10000)
+		result[clientID] = backend
+		cfg["clients."+clientID+".ip"] = "127.0.0.1"
+		cfg["clients."+clientID+".port"] = int64(backend.RealPort)
+		cfg["clients."+clientID+".timeout"] = int64(10000)
+		cfg["clients."+clientID+".timeoutPerAttempt"] = int64(10000)
 	}
 
 	return result, nil
