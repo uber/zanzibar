553d5f1121faf6d42b77e2bb8ea35f67a0d8dd90
Raynos <raynos2@gmail.com>
2017-06-14 16:33:01 -0700

    regenerate: update examples & golden files

--- examples/example-gateway/build/clients/bar/bar.go
+++ examples/example-gateway/build/clients/bar/bar.go
@@ -210,6 +210,53 @@ func (c *BarClient) Echo(
 	)
 }
 
+// ArgWithManyQueryParams calls "/bar/argWithManyQueryParams" endpoint.
+func (c *BarClient) ArgWithManyQueryParams(
+	ctx context.Context,
+	headers map[string]string,
+	r *clientsBarBar.Bar_ArgWithManyQueryParams_Args,
+) (*clientsBarBar.BarResponse, map[string]string, error) {
+
+	req := zanzibar.NewClientHTTPRequest(
+		c.ClientID, "argWithManyQueryParams", c.HTTPClient,
+	)
+
+	// Generate full URL.
+	fullURL := c.HTTPClient.BaseURL + "/bar" + "/argWithManyQueryParams"
+
+	err := req.WriteJSON("POST", fullURL, headers, r)
+	if err != nil {
+		return nil, nil, err
+	}
+	res, err := req.Do(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	respHeaders := map[string]string{}
+	for k := range res.Header {
+		respHeaders[k] = res.Header.Get(k)
+	}
+
+	res.CheckOKResponse([]int{200})
+
+	switch res.StatusCode {
+	case 200:
+		var responseBody clientsBarBar.BarResponse
+		err = res.ReadAndUnmarshalBody(&responseBody)
+		if err != nil {
+			return nil, respHeaders, err
+		}
+		// TODO(jakev): read response headers and put them in body
+
+		return &responseBody, respHeaders, nil
+	}
+
+	return nil, respHeaders, errors.Errorf(
+		"Unexpected http client response (%d)", res.StatusCode,
+	)
+}
+
 // ArgWithQueryParams calls "/bar/argWithQueryParams" endpoint.
 func (c *BarClient) ArgWithQueryParams(
 	ctx context.Context,
