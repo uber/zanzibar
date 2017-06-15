1f6b23079c17b40a9302a45a78cc0110de23eb26
Raynos <raynos2@gmail.com>
2017-06-14 17:42:47 -0700

    tests: add test for bool failing

--- test/endpoints/bar/bar_arg_with_query_params_test.go
+++ test/endpoints/bar/bar_arg_with_query_params_test.go
@@ -220,3 +220,56 @@ func TestBarWithManyQueryParamsCall(t *testing.T) {
 
 	assert.Equal(t, string(respBytes), compactStr(barResponseBytes))
 }
+
+func TestBarManyQueryParamsWithInvalidBool(t *testing.T) {
+	var counter int = 0
+
+	gateway, err := testGateway.CreateGateway(t, nil, &testGateway.Options{
+		KnownHTTPBackends: []string{"bar"},
+		TestBinary: filepath.Join(
+			getDirName(), "..", "..", "..",
+			"examples", "example-gateway", "build",
+			"services", "example-gateway", "main.go",
+		),
+	})
+	if !assert.NoError(t, err, "got bootstrap err") {
+		return
+	}
+	defer gateway.Close()
+
+	gateway.HTTPBackends()["bar"].HandleFunc(
+		"POST", "/bar/argWithManyQueryParams",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+			if _, err := w.Write([]byte(barResponseBytes)); err != nil {
+				t.Fatal("can't write fake response")
+			}
+			counter++
+		},
+	)
+
+	res, err := gateway.MakeRequest(
+		"GET",
+		"/bar/argWithManyQueryParams?"+
+			"aStr=foo&anOptStr=bar&aBool=t&anOptBool=false&"+
+			"aInt8=24&anOptInt8=-50&aInt16=48&anOptInt16=-100&"+
+			"aInt32=12&anOptInt32=-10&aInt64=4&anOptInt64=-1&"+
+			"aFloat64=5.1&anOptFloat64=-0.4",
+		nil, nil,
+	)
+	if !assert.NoError(t, err, "got http error") {
+		return
+	}
+
+	assert.Equal(t, "400 Bad Request", res.Status)
+	assert.Equal(t, 0, counter)
+
+	respBytes, err := ioutil.ReadAll(res.Body)
+	if !assert.NoError(t, err, "got http resp error") {
+		return
+	}
+
+	assert.Equal(t, string(respBytes), compactStr(`{
+		"error":"Could not parse query string"
+	}`))
+}
