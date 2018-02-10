80f101cbb4c80015be68018667472454e5a382b8
Chuntao Lu <clu@uber.com>
2018-02-09 17:14:33 -0800

    Add post generation script to generate mocks

--- codegen/runner/post-steps.sh
+++ codegen/runner/post-steps.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 set -e
+set -o pipefail
 
 if [ -z $1 ]; then
 	echo "prefix argument (\$1) is missing"
 	exit 1
 fi
 
-PREFIX="$1"
-DIRNAME="$(dirname $0)"
-EASY_JSON_RAW_DIR="$DIRNAME/../../scripts/easy_json"
-EASY_JSON_DIR="`cd "${EASY_JSON_RAW_DIR}";pwd`"
-EASY_JSON_FILE="$EASY_JSON_DIR/easy_json.go"
-EASY_JSON_BINARY="$EASY_JSON_DIR/easy_json"
-
 start=$(cat .TMP_ZANZIBAR_TIMESTAMP_FILE.txt)
 
-# TODO : Add new post steps :)
+BUILD_DIR="$1"
+
+DIRNAME="$(dirname "$0")"
+if [ -d "$DIRNAME/../../vendor" ]; then
+	MOCKERY_RAW_DIR="$DIRNAME/../../vendor/github.com/vektra/mockery"
+else
+	MOCKERY_RAW_DIR="$DIRNAME/../../../../../github.com/vektra/mockery"
+fi
+MOCKERY_DIR="$(cd "$MOCKERY_RAW_DIR";pwd)"
+MOCKERY_MAIN_FILE="$MOCKERY_DIR/cmd/mockery/mockery.go"
+MOCKERY_BINARY="$MOCKERY_DIR/cmd/mockery/mockery"
+
+go build -o "$MOCKERY_BINARY" "$MOCKERY_MAIN_FILE"
+
+"$MOCKERY_BINARY" -dir="$BUILD_DIR/clients" -inpkg -case=underscore -all > /dev/null
+end=$(date +%s)
+runtime=$((end-start))
+echo "Generated mock clients: +$runtime"
