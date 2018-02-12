77bc6f9f5fce4e09fb2f696b77e5f156496eb0f7
Rohit Patali <rohit.patali@uber.com>
2018-02-12 01:06:08 -0800

    use commit message accepted from the user
    existing commit message isn't very meaningful, instead accept a commit
    message from the user.

--- backend/repository/handler.go
+++ backend/repository/handler.go
@@ -441,7 +441,7 @@ func (h *Handler) GenerateDiff(w http.ResponseWriter, r *http.Request, _ httprou
 	req = &UpdateRequest{}
 	b, err := UnmarshalJSONBody(r, req)
 	if err != nil {
-		h.WriteErrorResponse(w, http.StatusBadRequest, errors.Wrap(err, "Failed to unmarshal body for createing a diff"))
+		h.WriteErrorResponse(w, http.StatusBadRequest, errors.Wrap(err, "Failed to unmarshal body for creating a diff"))
 		return
 	}
 	h.logger.Info("Generating a diff.",
@@ -466,12 +466,12 @@ func (h *Handler) GenerateDiff(w http.ResponseWriter, r *http.Request, _ httprou
 		return
 	}
 	if _, err := repo.LatestGatewayConfig(); err != nil {
-		h.WriteErrorResponse(w, http.StatusInternalServerError, errors.Wrap(err, "invalid gateway config after upadte"))
+		h.WriteErrorResponse(w, http.StatusInternalServerError, errors.Wrap(err, "invalid gateway config after update"))
 		return
 	}
 	diffReq := &DiffRequest{
 		BranchName:    strings.Join([]string{"batch", "update", currentTime()}, "_"),
-		CommitMessage: fmt.Sprintf("[%s] Batch update", gatewayConfig.ID),
+		CommitMessage: fmt.Sprintf("[%s] - %s", gatewayConfig.ID, *req.Description),
 	}
 	h.logger.Info("Generating diff...",
 		zap.String("localDir", repo.LocalDir()),
