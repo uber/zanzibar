6548befe0e7636c9f20246875cbf83cd67c4e69f
Chuntao Lu <clu@uber.com>
2018-02-15 15:39:19 -0800

    Add usage to post-scripts.sh

--- codegen/runner/post-steps.sh
+++ codegen/runner/post-steps.sh
@@ -2,20 +2,26 @@
 set -e
 set -o pipefail
 
+usage() {
+	echo "Usage: $0 [config-dir] [build-dir] "
+}
+
 if [ -z "$1" ]; then
-	echo "prefix argument (\$1) is missing"
+	echo "config dir argument (\$1) is missing"
+	usage
 	exit 1
 fi
 
 if [ -z "$2" ]; then
-	echo "config dir argument (\$2) is missing"
+	echo "build dir argument (\$2) is missing"
+	usage
 	exit 1
 fi
 
 start=$(cat .TMP_ZANZIBAR_TIMESTAMP_FILE.txt)
 
-BUILD_DIR="$1"
-CONFIG_DIR="$2"
+CONFIG_DIR="$1"
+BUILD_DIR="$2"
 
 DIRNAME="$(dirname "$0")"
 if [ -d "$DIRNAME/../../vendor" ]; then
