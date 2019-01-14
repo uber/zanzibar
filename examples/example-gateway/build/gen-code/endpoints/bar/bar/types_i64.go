33c75c3082ac8036501660ddec64903d1da0f609
r-patali <rohit.patali@uber.com>
2019-01-14 15:55:40 -0800

    add generated code with proper ordering

--- examples/example-gateway/build/gen-code/endpoints/bar/bar/types_i64.go
+++ examples/example-gateway/build/gen-code/endpoints/bar/bar/types_i64.go
@@ -8,29 +8,6 @@ import (
 	"time"
 )
 
-func (v Timestamp) MarshalJSON() ([]byte, error) {
-	x := (int64)(v)
-	return ([]byte)("\"" + time.Unix(x/1000, 0).UTC().Format(time.RFC3339) + "\""), nil
-}
-
-func (v *Timestamp) UnmarshalJSON(text []byte) error {
-	firstByte := text[0]
-	if firstByte == byte('"') {
-		x, err := time.Parse(time.RFC3339, string(text[1:len(text)-1]))
-		if err != nil {
-			return err
-		}
-		*v = Timestamp(x.Unix() * 1000)
-	} else {
-		x, err := strconv.ParseInt(string(text), 10, 64)
-		if err != nil {
-			return err
-		}
-		*v = Timestamp(x)
-	}
-	return nil
-}
-
 func (v Long) MarshalJSON() ([]byte, error) {
 	byteArray := make([]byte, 8, 8)
 	binary.BigEndian.PutUint64(byteArray, uint64(v))
@@ -57,6 +34,29 @@ func (v *Long) UnmarshalJSON(text []byte) error {
 			return err
 		}
 		*v = Long(x)
+	}
+	return nil
+}
+
+func (v Timestamp) MarshalJSON() ([]byte, error) {
+	x := (int64)(v)
+	return ([]byte)("\"" + time.Unix(x/1000, 0).UTC().Format(time.RFC3339) + "\""), nil
+}
+
+func (v *Timestamp) UnmarshalJSON(text []byte) error {
+	firstByte := text[0]
+	if firstByte == byte('"') {
+		x, err := time.Parse(time.RFC3339, string(text[1:len(text)-1]))
+		if err != nil {
+			return err
+		}
+		*v = Timestamp(x.Unix() * 1000)
+	} else {
+		x, err := strconv.ParseInt(string(text), 10, 64)
+		if err != nil {
+			return err
+		}
+		*v = Timestamp(x)
 	}
 	return nil
 }
