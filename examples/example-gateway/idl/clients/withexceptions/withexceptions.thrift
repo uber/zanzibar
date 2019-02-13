b6e27fa56877d6e389cbdfba0cbc06be68021c4e
Gregory Trowbridge <gregt@uber.com>
2019-02-13 11:14:07 -0800

    Compile withExceptions code.

--- examples/example-gateway/idl/clients/withexceptions/withexceptions.thrift
+++ examples/example-gateway/idl/clients/withexceptions/withexceptions.thrift
@@ -2,12 +2,16 @@ namespace java com.uber.zanzibar.clients.withexceptions
 
 
 exception ExceptionType1 {
-    1: string message
+    1: required string message
 }
 
 service WithExceptionsService {
     string func1(
     ) throws (
-        1: ExceptionType1 e1
+        1: ExceptionType1 e1 (zanzibar.http.status = "401")
+    ) (
+        zanzibar.http.method = "GET"
+        zanzibar.http.path = "/withexceptions/func1"
+        zanzibar.http.status = "200"
     )
 }
