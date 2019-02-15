cc211e15d03e564dec8158f0bfee1de0e683941c
Gregory Trowbridge <gregt@uber.com>
2019-02-14 17:02:23 -0800

    Update after generate.

--- examples/example-gateway/build/clients/bar/bar.go
+++ examples/example-gateway/build/clients/bar/bar.go
@@ -1321,7 +1321,7 @@ func (c *barClient) TooManyArgs(
 		respHeaders[k] = res.Header.Get(k)
 	}
 
-	res.CheckOKResponse([]int{200, 418, 403})
+	res.CheckOKResponse([]int{200, 403, 418})
 
 	switch res.StatusCode {
 	case 200:
