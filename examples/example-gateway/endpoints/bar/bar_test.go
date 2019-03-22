e96a0565aa587f4b2ceb86476308fd324157552d
Abhishek Parwal <abhishek.parwal@uber.com>
2019-03-21 19:11:13 -0700

    test cases

--- examples/example-gateway/endpoints/bar/bar_test.go
+++ examples/example-gateway/endpoints/bar/bar_test.go
@@ -1,15 +1,30 @@
 package contacts_test
 
 import (
+	"context"
+	"net/http"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/uber/zanzibar/examples/example-gateway/build/gen-code/clients/bar/bar"
+	clientsBarBar "github.com/uber/zanzibar/examples/example-gateway/build/gen-code/clients/bar/bar"
+	exampleGateway "github.com/uber/zanzibar/examples/example-gateway/build/services/example-gateway"
 	ms "github.com/uber/zanzibar/examples/example-gateway/build/services/example-gateway/mock-service"
-)
+	benchGateway "github.com/uber/zanzibar/test/lib/bench_gateway"
+	testGateway "github.com/uber/zanzibar/test/lib/test_gateway"
+	"github.com/uber/zanzibar/test/lib/util"
+git )
 
-func TestBarListAndEnum(t *testing.T) {
+var defaultTestOptions = &testGateway.Options{
+	KnownHTTPBackends: []string{"bar"},
+	ConfigFiles:       util.DefaultConfigFiles("example-gateway"),
+}
+var defaultTestConfig = map[string]interface{}{
+	"clients.bar.serviceName": "bar",
+}
+
+func TestBarListAndEnumEndpoint(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
@@ -23,11 +38,48 @@ func TestBarListAndEnum(t *testing.T) {
 	ms.MockClients().Bar.EXPECT().ListAndEnum(gomock.Any(), gomock.Any(), args).Return("demo", nil, nil).AnyTimes()
 
 	res, err := ms.MakeHTTPRequest(
-		"GET", "/bar/list-and-enum?demoIds[]=abc&demoIds[]=def&demoType=0", nil, nil,
+		"GET", "/bar/list-and-enum?demoIds=abc&demoIds=def&demoType=0", nil, nil,
 	)
 	if !assert.NoError(t, err, "got http error") {
 		return
 	}
 
 	assert.Equal(t, 200, res.StatusCode)
+}
+
+func TestBarListAndEnumClient(t *testing.T) {
+	gateway, err := benchGateway.CreateGateway(
+		defaultTestConfig,
+		defaultTestOptions,
+		exampleGateway.CreateGateway,
+	)
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer gateway.Close()
+
+	bgateway := gateway.(*benchGateway.BenchGateway)
+
+	bgateway.HTTPBackends()["bar"].HandleFunc(
+		"GET", "/bar/list-and-enum",
+		func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, []string{"abc", "def"}, r.URL.Query()["demoIds"])
+			assert.Equal(t, "0", r.URL.Query().Get("demoType"))
+			w.WriteHeader(200)
+			_, _ = w.Write([]byte(`"test-response"`))
+		},
+	)
+
+	deps := bgateway.Dependencies.(*exampleGateway.DependenciesTree)
+	bClient := deps.Client.Bar
+
+	demoType := bar.DemoTypeFirst
+	body, _, err := bClient.ListAndEnum(
+		context.Background(), nil, &clientsBarBar.Bar_ListAndEnum_Args{DemoIds: []string{"abc", "def"}, DemoType: &demoType},
+	)
+	assert.NoError(t, err)
+	assert.NotNil(t, body)
+
+	logs := bgateway.AllLogs()
+	assert.Len(t, logs["Finished an outgoing client HTTP request"], 1)
 }
