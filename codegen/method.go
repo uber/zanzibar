6455da68aa6e60362f7f1a57644a3216485d5502
Aravind Gopalan <50342444+argouber@users.noreply.github.com>
2019-08-20 23:57:23 -0700

    Argo/query params for all methods (#625)
    Allow query params on ALL http methods
    
    Nothing in the standards definitions preclude query params for methods other than GET
    We have use-cases where an endpoint wanted to use DELETE with query params and that was used by the downstream service.
    
    With this change, a few things:
     - Allowed other methods to process query params
     - Fixed the test-cases -- all query params need to have the http.ref = "query.xxx" annotation
       In the test cases, we were grandfathering non-annotated fields in the struct as query params since these were only for GET and it worked well
     - One test case added for delete with query params

--- codegen/method.go
+++ codegen/method.go
@@ -238,7 +238,7 @@ func NewMethod(
 
 	method.setValidStatusCodes()
 
-	if method.HTTPMethod == "GET" && method.RequestType != "" {
+	if method.RequestType != "" {
 		if method.IsEndpoint {
 			err := method.setParseQueryParamStatements(funcSpec, packageHelper)
 			if err != nil {
@@ -1132,13 +1132,19 @@ func (ms *MethodSpec) setWriteQueryParamStatements(
 ) error {
 	var statements LineBuilder
 	var hasQueryFields bool
-	var stack = []string{}
+	var stack []string
+	isVoidReturn := funcSpec.ResultSpec.ReturnType == nil
 
 	visitor := func(
 		goPrefix string, thriftPrefix string, field *compile.FieldSpec,
 	) bool {
 		realType := compile.RootTypeSpec(field.Type)
 		longFieldName := goPrefix + "." + PascalCase(field.Name)
+
+		httpRefAnnotation := field.Annotations[ms.annotations.HTTPRef]
+		if !strings.HasPrefix(httpRefAnnotation, "query") {
+			return false
+		}
 
 		if len(stack) > 0 {
 			if !strings.HasPrefix(longFieldName, stack[len(stack)-1]) {
@@ -1152,24 +1158,23 @@ func (ms *MethodSpec) setWriteQueryParamStatements(
 
 			if field.Required {
 				statements.appendf("if r%s == nil {", longFieldName)
-				// TODO: generate correct number of nils...
-				statements.append("\treturn nil, nil, errors.New(")
+				// Generate correct number of nils...
+				if isVoidReturn {
+					statements.append("\treturn nil, errors.New(")
+				} else {
+					statements.append("\treturn nil, nil, errors.New(")
+				}
 				statements.appendf("\t\t\"The field %s is required\",",
 					longFieldName,
 				)
 				statements.append("\t)")
-				statements.appendf("}")
+				statements.append("}")
 			} else {
 				stack = append(stack, longFieldName)
 
 				statements.appendf("if r%s != nil {", longFieldName)
 			}
 
-			return false
-		}
-
-		httpRefAnnotation := field.Annotations[ms.annotations.HTTPRef]
-		if httpRefAnnotation != "" && !strings.HasPrefix(httpRefAnnotation, "query") {
 			return false
 		}
 
@@ -1250,6 +1255,11 @@ func (ms *MethodSpec) setParseQueryParamStatements(
 		longFieldName := goPrefix + "." + PascalCase(field.Name)
 		longQueryName := ms.getLongQueryName(field, thriftPrefix)
 
+		httpRefAnnotation := field.Annotations[ms.annotations.HTTPRef]
+		if !strings.HasPrefix(httpRefAnnotation, "query") {
+			return false
+		}
+
 		if len(stack) > 0 {
 			if !strings.HasPrefix(longFieldName, stack[len(stack)-1]) {
 				stack = stack[:len(stack)-1]
@@ -1324,11 +1334,6 @@ func (ms *MethodSpec) setParseQueryParamStatements(
 			return false
 		}
 		identifierName := CamelCase(longQueryName) + "Query"
-
-		httpRefAnnotation := field.Annotations[ms.annotations.HTTPRef]
-		if httpRefAnnotation != "" && !strings.HasPrefix(httpRefAnnotation, "query") {
-			return false
-		}
 
 		okIdentifierName := CamelCase(longQueryName) + "Ok"
 		if field.Required {
