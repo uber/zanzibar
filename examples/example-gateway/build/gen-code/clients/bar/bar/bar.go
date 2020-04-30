76c1cb046e209376ecb456c7836a3f74e3d9a7ab
Ronald Hsu <hothero0705@gmail.com>
2020-05-01 01:52:53 +0800

    manually fix generated code

--- examples/example-gateway/build/gen-code/clients/bar/bar/bar.go
+++ examples/example-gateway/build/gen-code/clients/bar/bar/bar.go
@@ -19322,10 +19322,12 @@ func init() {
 		Key   *BarResponse
 		Value string
 	}, err error) {
+
 		if result.Success != nil {
 			success = result.Success
 			return
 		}
+
 		err = errors.New("expected a non-void result")
 		return
 	}
--- examples/example-gateway/build/gen-code/clients/baz/baz/baz.go
+++ examples/example-gateway/build/gen-code/clients/baz/baz/baz.go
@@ -8262,10 +8262,12 @@ func init() {
 		Key   *base.BazResponse
 		Value string
 	}, err error) {
+
 		if result.Success != nil {
 			success = result.Success
 			return
 		}
+
 		err = errors.New("expected a non-void result")
 		return
 	}
