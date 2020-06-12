4fea7f7bd1cb762c8f88a010628f29c2f25e0907
Steven Bauer <bauer@uber.com>
2020-06-12 09:32:52 -0700

    missing comma

--- runtime/server_http_response.go
+++ runtime/server_http_response.go
@@ -133,7 +133,7 @@ func serverHTTPLogFields(req *ServerHTTPRequest, res *ServerHTTPResponse) []zapc
 		zap.String("host", req.httpRequest.Host),
 		zap.Time("timestamp-started", req.startTime),
 		zap.Time("timestamp-finished", res.finishTime),
-		zap.Int("statusCode", res.StatusCode)
+		zap.Int("statusCode", res.StatusCode),
 	}
 
 	for k, v := range res.Headers() {
