b5a90be3846d8b6440cd489d2448cac1c7089557
Aman Garg <aman.garg@uber.com>
2022-02-12 17:57:50 +0530

    Add non blocking lifecycle hook instead of blocking wait()

--- examples/example-gateway/build/services/example-gateway/main/main.go
+++ examples/example-gateway/build/services/example-gateway/main/main.go
@@ -24,11 +24,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+
+	"github.com/pkg/errors"
 
 	_ "go.uber.org/automaxprocs"
 	"go.uber.org/fx"
@@ -102,17 +105,40 @@ func main() {
 	).Run()
 }
 
-func zanzibarMain() {
+type Params struct {
+	fx.In
+	Lifecycle fx.Lifecycle
+}
+
+func zanzibarMain(p Params) {
 	readFlags()
 	server, err := createGateway()
 	if err != nil {
-		panic(err)
+		panic(errors.Wrap(err, "failed to create gateway server"))
 	}
 
-	err = server.Bootstrap()
-	if err != nil {
-		panic(err)
-	}
-
-	logAndWait(server)
+	p.Lifecycle.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			err = server.Bootstrap()
+			if err != nil {
+				panic(errors.Wrap(err, "failed to bootstrap gateway server"))
+			}
+			server.Logger.Info("started example-gateway",
+				zap.String("realHTTPAddr", server.RealHTTPAddr),
+				zap.String("realTChannelAddr", server.RealTChannelAddr),
+				zap.Any("config", server.InspectOrDie()),
+			)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			if server == nil {
+				return nil
+			}
+			server.Logger.Info("fx OnStop() hook activated")
+			server.WaitGroup.Add(1)
+			server.Shutdown()
+			server.WaitGroup.Done()
+			return nil
+		},
+	})
 }
